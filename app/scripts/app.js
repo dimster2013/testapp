842214948dc54eac75f20cb5f4b36ad2c9a28791
Teddy <Teddy@mail.com>
2015-04-23 14:07:06 +1000

    added sorting filters etc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,41 +1,23 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name yomanApp
- * @description
- * # yomanApp
- *
- * Main module of the application.
- */
 angular
-  .module('yomanApp', [
+  .module('vodaApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch'
+
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/issues', {
-        templateUrl: 'views/issues.html',
-        controller: 'IssuesCtrl'
-      })
-      .when('/accordeon', {
-        templateUrl: 'views/accordeon.html',
-        controller: 'IssuesCtrl'
+      .when('/richlist', {
+        templateUrl: 'views/richlist.html',
+        controller: 'RichListCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  }).
+  constant('_',window._);
